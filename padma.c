154fff47538dd3ebe56beaf048db30d5ca38712d
Padmapriya241 <78944805+Padmapriya241@users.noreply.github.com>
2021-02-18 20:01:03 -0800

    wlcome

---
+++ padma.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=0,j=0,k=0,mid=0,l=0;
+char a[10];
+printf("enter string");
+scanf("%s",a);
+l=strnlen(a);
+for(i=0;i<=1,i++)
+{
+j=1/2;
+for(k=0;k<i;k++)
+{
+if(k<=1/2)
+{
+printf("%c",a[j]);
+j++;
+else
+{
+j=(k-(1/2+1);
+printf("%c",a[j]);
+j++;
+printf("/n");
+}
+return 0;
+}
